1301d17e6ab287979a215fe8623d01344ae96431
KonekkoNekko <nabiel.setiawan@gmail.com>
2024-11-10 21:08:38 +0700

    first commit

---
+++ resources/views/mail/orders/confirmed.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+    Hi {{ $booking->name }}, terima kasih telah memesan tiket wisata di JuaraTiket, kami sedang memeriksa pembayaran
+    anda saat ini. Anda dapat memeriksa secara berkala pada website kami dan berikut adalah ID booking transaction anda:
+    {{ $booking->booking_trx_id }}
+
+    <x-mail::button :url="route('front.check_booking')">
+        Check Booking
+    </x-mail::button>
+
+    Thanks <br>
+    {{ config('app.name') }}
+</x-mail::message>
